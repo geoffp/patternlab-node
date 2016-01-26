3c5c2e1b75832b100a0bd01b83d03d3599f71e4a
Geoffrey Pursell <geoffrey.pursell@target.com>
2016-01-26 16:19:32 -0600

    Underscore engine, tests, and basic test pattern tree copied from handlebars engine. All tests failing.

---
+++ builder/pattern_engines/engine_underscore.js
@@ -0,0 +1,70 @@
+/*
+ * underscore pattern engine for patternlab-node - v0.15.1 - 2015
+ *
+ * Geoffrey Pursell, Brian Muenzenmeyer, and the web community.
+ * Licensed under the MIT license.
+ *
+ * Many thanks to Brad Frost and Dave Olsen for inspiration, encouragement, and advice.
+ *
+ */
+
+(function () {
+  "use strict";
+
+  var _ = require('underscore');
+
+  var engine_underscore = {
+    engine: _,
+    engineName: 'underscore',
+    engineFileExtension: '.underscore',
+
+    // partial expansion is only necessary for Mustache templates that have
+    // style modifiers or pattern parameters (I think)
+    expandPartials: false,
+
+    // regexes, stored here so they're only compiled once
+    findPartialsRE: null, // TODO,
+    findPartialsWithStyleModifiersRE: null, // TODO
+    findPartialsWithPatternParametersRE: null, // TODO
+    findListItemsRE: /({{#( )?)(list(I|i)tems.)(one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen|sixteen|seventeen|eighteen|nineteen|twenty)( )?}}/g,
+
+    // render it
+    renderPattern: function renderPattern(template, data, partials) {
+      if (partials) {
+        _.registerPartial(partials);
+      }
+      var compiled = _.compile(template);
+      return compiled(data);
+    },
+
+    registerPartial: function (oPattern) {
+      _.registerPartial(oPattern.key, oPattern.template);
+    },
+
+    // find and return any {{> template-name }} within pattern
+    findPartials: function findPartials(pattern) {
+      var matches = pattern.template.match(this.findPartialsRE);
+      return matches;
+    },
+    findPartialsWithStyleModifiers: function(pattern) {
+      return [];
+    },
+    // returns any patterns that match {{> value(foo:"bar") }} or {{>
+    // value:mod(foo:"bar") }} within the pattern
+    findPartialsWithPatternParameters: function(pattern) {
+      return [];
+    },
+    findListItems: function(pattern) {
+      var matches = pattern.template.match(this.findListItemsRE);
+      return matches;
+    },
+    // given a pattern, and a partial string, tease out the "pattern key" and
+    // return it.
+    findPartialKey: function(partialString) {
+      var partialKey = partialString.replace(this.findPartialsRE, '$1');
+      return partialKey;
+    }
+  };
+
+  module.exports = engine_underscore;
+})();
---
+++ test/engine_underscore_tests.js
@@ -0,0 +1,227 @@
+(function () {
+  "use strict";
+
+  var path = require('path');
+  var pa = require('../builder/pattern_assembler');
+  var object_factory = require('../builder/object_factory');
+  var testPatternsPath = path.resolve(__dirname, 'files', '_underscore-test-patterns');
+
+  try {
+    require('underscore');
+  } catch (err) {
+    console.log('underscore renderer not installed; skipping tests');
+    return;
+  }
+
+  // fake pattern lab constructor:
+  // sets up a fake patternlab object, which is needed by the pattern processing
+  // apparatus.
+  function fakePatternLab() {
+    var fpl = {
+      partials: {},
+      patterns: [],
+      footer: '',
+      header: '',
+      listitems: {},
+      listItemArray: [],
+      data: {
+        link: {}
+      },
+      config: require('../config.json'),
+      package: {}
+    };
+
+    // patch the pattern source so the pattern assembler can correctly determine
+    // the "subdir"
+    fpl.config.paths.source.patterns = testPatternsPath;
+
+    return fpl;
+  }
+
+
+  // function for testing sets of partials
+  function testFindPartials(test, partialTests) {
+    test.expect(partialTests.length + 1);
+
+    // setup current pattern from what we would have during execution
+    // docs on partial syntax are here:
+    // http://patternlab.io/docs/pattern-including.html
+    var currentPattern = object_factory.oPattern.create(
+      '/home/fakeuser/pl/source/_patterns/01-molecules/00-testing/00-test-mol.underscore', // abspath
+      '01-molecules\\00-testing', // subdir
+      '00-test-mol.underscore', // filename,
+      null, // data
+      {
+        template: partialTests.join()
+      }
+    );
+
+    // act
+    var results = currentPattern.findPartials();
+
+    // assert
+    test.equals(results.length, partialTests.length);
+    partialTests.forEach(function(testString, index) {
+      test.equals(results[index], testString);
+    });
+
+    test.done();
+  }
+
+  exports['engine_underscore'] = {
+    'hello world underscore pattern renders': function (test) {
+      test.expect(1);
+
+      var patternPath = path.resolve(
+        testPatternsPath,
+        '00-atoms',
+        '00-global',
+        '00-helloworld.underscore'
+      );
+
+      // do all the normal processing of the pattern
+      var patternlab = new fakePatternLab();
+      var assembler = new pa();
+      var helloWorldPattern = assembler.process_pattern_iterative(patternPath, patternlab);
+      assembler.process_pattern_recursive(patternPath, patternlab);
+
+      test.equals(helloWorldPattern.render(), 'Hello world!\n');
+      test.done();
+    },
+    'hello worlds underscore pattern can see the atoms-helloworld partial and renders it twice': function (test) {
+      test.expect(1);
+
+      // pattern paths
+      var pattern1Path = path.resolve(
+        testPatternsPath,
+        '00-atoms',
+        '00-global',
+        '00-helloworld.underscore'
+      );
+      var pattern2Path = path.resolve(
+        testPatternsPath,
+        '00-molecules',
+        '00-global',
+        '00-helloworlds.underscore'
+      );
+
+      // set up environment
+      var patternlab = new fakePatternLab(); // environment
+      var assembler = new pa();
+
+      // do all the normal processing of the pattern
+      assembler.process_pattern_iterative(pattern1Path, patternlab);
+      var helloWorldsPattern = assembler.process_pattern_iterative(pattern2Path, patternlab);
+      assembler.process_pattern_recursive(pattern1Path, patternlab);
+      assembler.process_pattern_recursive(pattern2Path, patternlab);
+
+      // test
+      test.equals(helloWorldsPattern.render(), 'Hello world!\n and Hello world!\n\n');
+      test.done();
+    },
+    'underscore partials can render JSON values': function (test) {
+      test.expect(1);
+
+      // pattern paths
+      var pattern1Path = path.resolve(
+        testPatternsPath,
+        '00-atoms',
+        '00-global',
+        '00-helloworld-withdata.underscore'
+      );
+
+      // set up environment
+      var patternlab = new fakePatternLab(); // environment
+      var assembler = new pa();
+
+      // do all the normal processing of the pattern
+      var helloWorldWithData = assembler.process_pattern_iterative(pattern1Path, patternlab);
+      assembler.process_pattern_recursive(pattern1Path, patternlab);
+
+      // test
+      test.equals(helloWorldWithData.render(), 'Hello world!\nYeah, we got the subtitle from the JSON.\n');
+      test.done();
+    },
+    'underscore partials use the JSON environment from the calling pattern and can accept passed parameters': function (test) {
+      test.expect(1);
+
+      debugger;
+
+      // pattern paths
+      var atomPath = path.resolve(
+        testPatternsPath,
+        '00-atoms',
+        '00-global',
+        '00-helloworld-withdata.underscore'
+      );
+      var molPath = path.resolve(
+        testPatternsPath,
+        '00-molecules',
+        '00-global',
+        '00-call-atom-with-molecule-data.underscore'
+      );
+
+      // set up environment
+      var patternlab = new fakePatternLab(); // environment
+      var assembler = new pa();
+
+      // do all the normal processing of the pattern
+      var atom = assembler.process_pattern_iterative(atomPath, patternlab);
+      var mol = assembler.process_pattern_iterative(molPath, patternlab);
+      assembler.process_pattern_recursive(atomPath, patternlab);
+      assembler.process_pattern_recursive(molPath, patternlab);
+
+      // test
+      test.equals(mol.render(), '<h2>Call with default JSON environment:</h2>\nThis is Hello world!\nfrom the default JSON.\n\n\n<h2>Call with passed parameter:</h2>\nHowever, this is Hello world!\nfrom a totally different blob.\n\n');
+      test.done();
+    },
+    'find_pattern_partials finds partials': function(test){
+      testFindPartials(test, [
+        "{{> molecules-comment-header}}",
+        "{{>  molecules-comment-header}}",
+        "{{> \n	molecules-comment-header\n}}",
+        "{{>  molecules-weird-spacing     }}",
+        "{{>  molecules-ba_d-cha*rs     }}"
+      ]);
+    },
+    'find_pattern_partials finds verbose partials': function(test){
+      testFindPartials(test, [
+        '{{> 01-molecules/06-components/03-comment-header.underscore }}',
+        "{{> 01-molecules/06-components/02-single-comment.underscore(description: 'A life is like a garden. Perfect moments can be had, but not preserved, except in memory.') }}",
+        '{{> molecules-single-comment:foo }}',
+        "{{>atoms-error(message: 'That\'s no moon...')}}",
+        "{{> atoms-error(message: 'That\'s no moon...') }}",
+        '{{> 00-atoms/00-global/06-test }}'
+      ]);
+    },
+    'find_pattern_partials finds simple partials with parameters': function(test){
+      testFindPartials(test, [
+        "{{> molecules-single-comment(description: 'A life isn\'t like a garden. Perfect moments can be had, but not preserved, except in memory.') }}",
+        '{{> molecules-single-comment(description:"A life is like a \"garden\". Perfect moments can be had, but not preserved, except in memory.") }}'
+      ]);
+    },
+    'find_pattern_partials finds simple partials with style modifiers': function(test){
+      testFindPartials(test, [
+        '{{> molecules-single-comment:foo }}'
+      ]);
+    },
+    'find_pattern_partials finds partials with underscore parameters': function(test){
+      testFindPartials(test, [
+        '{{> atoms-title title="bravo" headingLevel="2" headingSize="bravo" position="left"}}',
+        '{{> atoms-title title="bravo"\n  headingLevel="2"\n  headingSize="bravo"\n  position="left"}}',
+        '{{> atoms-title title="color &nbsp;<span style=\'font-weight:normal\'>midnight blue</span>" headingSize="charlie"}}',
+        '{{> atoms-input label="city" required=true}}',
+        '{{> organisms-product-filter filterData}}',
+        '{{> atoms-input email required=true}}',
+        '{{> molecules-storycard variants.flex }}',
+        '{{> myPartial name=../name }}'
+      ]);
+    },
+
+    'find_pattern_partials finds underscore block partials': function(test){
+      testFindPartials(test, [
+        '{{#> myPartial }}'
+      ]);
+    }
+  };
+})();
